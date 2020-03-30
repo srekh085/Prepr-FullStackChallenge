724304389c5d2c0b92843ccce4b09de487e922f8
srekh085@uottawa.ca <srekh085@uottawa.ca>
2020-03-30 02:28:47 -0400

    Screenshots Added

---
+++ preprLabLocator/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8" style="text-align: center">
+            Click on Lab Markers for more information!
+        </div>
+</div>
+    <div class="col-lg-12" style="height: 80vh" >
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1jULQ__3JL_RMa_jhJGEHz6mtPzPBe2QJ" width="100%" height="100%"></iframe>
+    </div>
+</div>
+@endsection
